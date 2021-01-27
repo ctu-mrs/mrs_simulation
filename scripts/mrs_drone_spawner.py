a05c3fd6f56fe20a83595ef77abdc1e3e997d8eb
stibipet <stibipet@fel.cvut.cz>
2021-01-27 15:05:54 +0100

    [DroneSpawner]: update process order, wait for firmware

--- scripts/mrs_drone_spawner.py
+++ scripts/mrs_drone_spawner.py
@@ -67,6 +67,7 @@ class MrsDroneSpawner():
         # #{ setup system variables
         self.spawn_called = False
         self.processing = False
+        self.mavros_connected = False
         self.process_queue = []
         self.process_queue_mutex = threading.Lock()
         self.active_vehicles = []
@@ -191,6 +192,13 @@ class MrsDroneSpawner():
         self.diagnostics_pub.publish(diagnostics)
     # #}
 
+    # #{ callback_mavros_state
+    def callback_mavros_state(self, msg):
+        if msg.connected:
+            self.mavros_connected = True
+            self.mavros_state_sub.unregister()
+    # #}
+
     # #{ callback_model_states
     def callback_model_states(self, msg):
         uav_models = []
@@ -259,8 +267,8 @@ class MrsDroneSpawner():
             ID = params_dict['uav_ids'][i]
             self.queued_vehicles.append('uav' + str(ID))
             self.process_queue.append((self.launch_mavros, (ID, uav_roslaunch_args)))
-            self.process_queue.append((self.launch_firmware, (ID, uav_roslaunch_args)))
             self.process_queue.append((self.spawn_simulation_model, (ID, uav_roslaunch_args)))
+            self.process_queue.append((self.launch_firmware, (ID, uav_roslaunch_args)))
         self.process_queue_mutex.release()
         # #}
 
@@ -539,6 +547,7 @@ class MrsDroneSpawner():
 
     # #{ launch_firmware
     def launch_firmware(self, ID, uav_roslaunch_args):
+        self.mavros_connected = False
         rinfo('Running firmware for uav' + str(ID) + '...')
         uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
         roslaunch.configure_logging(uuid)
@@ -551,6 +560,11 @@ class MrsDroneSpawner():
             launch.shutdown()
             return None
 
+        self.mavros_state_sub = rospy.Subscriber('/uav' + str(ID) + '/mavros/state', MavrosState, self.callback_mavros_state)
+        while not self.mavros_connected:
+            rospy.sleep(0.05)
+        
+        self.mavros_connected = False
         rinfo('Firmware for uav' + str(ID) + ' started!')
         return launch
     # #}
